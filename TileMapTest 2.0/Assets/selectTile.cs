1a7fff35050a8e1501fe082c34648d168713f1f4
Stein Bergervoet <stein.bergervoet@gmail.com>
2017-12-13 13:08:17 +0100

    Quad's voor selectie.
    3D map met quad die gebruikt gaat worden voor selectie van tiles.

---
+++ TileMapTest 2.0/Assets/selectTile.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class selectTile : MonoBehaviour {
+    
+
+    public void OnMouseOver() {
+        if (Input.GetMouseButtonDown(1))
+
+    }
+}
