ab9210905893bc5d7471bbda4743cd74ef6b936f
Stein Bergervoet <stein.bergervoet@gmail.com>
2017-12-13 12:09:35 +0100

    3D-map. Singleplayer basis.
    Een 3D-map gemaakt van verschillende soorten cubes (Plais, Mountain & Lake).

---
+++ TileMapTest 2.0/Assets/_Scripts/TileMap.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class TileMap : MonoBehaviour {
+
+    public TileType[] tileTypes;
+    public GameObject[,] tileObjects;
+    int[,] tiles;
+    int mapSizeX = 10, mapSizeY = 10;
+
+    void Start() {
+        tileObjects = new GameObject[mapSizeX, mapSizeY];
+        tiles = new int[mapSizeX, mapSizeY];
+
+        AllocateMap();
+        GenerateMap();
+    }
+
+    public void AllocateMap() {
+        for (int x = 0; x < mapSizeX; x++)
+            for (int z = 0; z < mapSizeY; z++)
+                tiles[x, z] = TileType.TYPE_PLAIN;
+
+        for (int x = 2; x < 4; x++)
+            for (int z = 4; z < 6; z++)
+                tiles[x, z] = TileType.TYPE_MOUNTAIN;
+
+        for (int x = 6; x < 8; x++)
+            for (int z = 4; z < 6; z++)
+                tiles[x, z] = TileType.TYPE_LAKE;
+    }
+
+    public void GenerateMap() {
+        for (int x = 0; x < mapSizeX; x++)
+            for (int z = 0; z < mapSizeY; z++)
+                tileObjects[x, z] = Instantiate(tileTypes[tiles[x, z]].tileVisualPrefab, new Vector3(x, 0, z), Quaternion.identity);
+    }
+}
