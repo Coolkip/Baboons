3ae824de7c153ed0af39a0ff73701fc53ffe4808
Stein Bergervoet <stein.bergervoet@gmail.com>
2017-12-14 16:20:46 +0100

    Selecten van tiles
    Selecteren van tiles. Groen voor walkable en rood voor not walkable.

---
+++ TileMapTest 2.0/Assets/_Scripts/SelectTile.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectTile : MonoBehaviour {
+    public Transform tile;
+
+    public void OnMouseOver() {
+        TileMap tileMap = GameObject.FindWithTag("Overworld").GetComponent<TileMap>();
+
+        if (Input.GetMouseButtonDown(0)) {
+
+            if (tileMap.selectionObjects[(int)tile.position.x, (int)tile.position.z].activeSelf)
+                tileMap.selectionObjects[(int)tile.position.x, (int)tile.position.z].SetActive(false);
+            else {
+                for (int x = 0; x < tileMap.mapSizeX; x++) {
+                    for (int z = 0; z < tileMap.mapSizeY; z++) {
+                        if (tileMap.selectionObjects[x, z].activeSelf)
+                            tileMap.selectionObjects[x, z].SetActive(false);
+                        if (!tileMap.selectionObjects[x, z].activeSelf)
+                            tileMap.selectionObjects[(int)tile.position.x, (int)tile.position.z].SetActive(true);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ TileMapTest 2.0/Assets/_Scripts/TileMap.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Networking;
+using UnityEngine;
+
+public class TileMap : MonoBehaviour {
+    public TileType[] tileTypes;
+    public GameObject[,] tileObjects;
+    int[,] tiles;
+
+    public int mapSizeX = 10, mapSizeY = 10;
+
+    public SelectionType[] selectionTypes;
+    public GameObject[,] selectionObjects;
+    int[,] selections;
+
+    void Start() {
+        tileObjects = new GameObject[mapSizeX, mapSizeY];
+        selectionObjects = new GameObject[mapSizeX, mapSizeY];
+
+        tiles = new int[mapSizeX, mapSizeY];
+        selections = new int[mapSizeX, mapSizeY];
+
+        AllocateMap();
+        GenerateMap();
+    }
+
+    public void AllocateMap() {
+        for (int x = 0; x < mapSizeX; x++) {
+            for (int z = 0; z < mapSizeY; z++) {
+                    tiles[x, z] = TileType.TYPE_PLAIN;
+                    selections[x, z] = SelectionType.TYPE_WALKABLE;
+            }                
+        }
+            
+        for (int x = 2; x < 4; x++)
+            for (int z = 4; z < 6; z++)
+                tiles[x, z] = TileType.TYPE_MOUNTAIN;
+
+        for (int x = 6; x < 8; x++) {
+            for (int z = 4; z < 6; z++) {
+                tiles[x, z] = TileType.TYPE_LAKE;
+                selections[x, z] = SelectionType.TYPE_NOTWALKABLE;
+            }                
+        }            
+    }
+
+    public void GenerateMap() {
+        for (int x = 0; x < mapSizeX; x++) {
+            for (int z = 0; z < mapSizeY; z++) {
+                tileObjects[x, z] = Instantiate(tileTypes[tiles[x, z]].tileVisualPrefab, new Vector3(x, 0, z), Quaternion.identity);
+                selectionObjects[x, z] = Instantiate(selectionTypes[selections[x, z]].selectionVisualPrefab, new Vector3(x, 0.51f, z), Quaternion.Euler(90, 0, 0));
+                selectionObjects[x, z].SetActive(false);
+            }
+        }
+    }
+}
